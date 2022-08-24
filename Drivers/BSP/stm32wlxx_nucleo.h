74503f4a4ce059059b79ffb399ddc3dc07cac1da
Alessio Moro <alessio.m989@gmail.com>
2022-08-24 17:53:33 +0200

    First commit

---
+++ Drivers/BSP/stm32wlxx_nucleo.h
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * @file    stm32wlxx_nucleo.h
+  * @author  Alessio Moro.
+  * @brief   Dummy file for fixing reference by STM32CubeMX genetared code.
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32WLXX_NUCLEO_H
+#define STM32WLXX_NUCLEO_H
+
+#endif /* STM32WLXX_NUCLEO_H */
